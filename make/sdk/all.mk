b03c3cf3ef1d95abdbf409e5b7e5cb1816992b69
Wael Jammal <wael.jammal@azarc.io>
2022-11-03 19:30:06 +0000

    VTHP-883: gen grpc go module

--- make/sdk/all.mk
+++ make/sdk/all.mk
@@ -7,9 +7,16 @@ BUF_BREAKING_AGAINST_INPUT ?= .git\#branch=main
 BUF_FORMAT_INPUT := .
 BUF_VERSION ?= v1.9.0
 
+GO_GET_PKGS := $(GO_GET_PKGS) \
+	github.com/srikrsna/protoc-gen-gotag@v0.6.2 \
+	google.golang.org/grpc/cmd/protoc-gen-go-grpc@v1.1.0 \
+	github.com/pseudomuto/protoc-gen-doc/cmd/protoc-gen-doc@v1.5.0 \
+	github.com/envoyproxy/protoc-gen-validate@v0.6.2
+
 include make/go/bootstrap.mk
 include make/go/buf.mk
 include make/go/go.mk
+include make/go/dep_protoc.mk
 include make/go/dep_protoc_gen_go.mk
 
 bufgeneratedeps:: $(BUF) $(PROTOC_GEN_GO)
