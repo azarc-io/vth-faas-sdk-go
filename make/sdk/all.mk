ad5a0db1aa10b69f030ff9eef61e8a43149cb7af
Wael Jammal <wael.jammal@azarc.io>
2022-11-04 07:29:19 +0000

    Add go releaser dep downloader

--- make/sdk/all.mk
+++ make/sdk/all.mk
@@ -1,6 +1,3 @@
-GO_BINS := $(GO_BINS) cmd/foo
-DOCKER_BINS := $(DOCKER_BINS) foo
-
 BUF_LINT_INPUT := .
 BUF_BREAKING_INPUT := .
 BUF_BREAKING_AGAINST_INPUT ?= .git\#branch=main
@@ -19,6 +16,9 @@ include make/go/go.mk
 include make/go/dep_protoc.mk
 include make/go/dep_protoc_gen_go.mk
 include make/go/dep_protoc_gen_go_grpc.mk
+include make/go/dep_go_releaser.mk
+
+# HOOKS
 
 bufgeneratedeps:: $(BUF) $(PROTOC_GEN_GO) $(PROTOC_GEN_GO_GRPC)
 
@@ -33,10 +33,10 @@ bufgeneratesteps::
 
 # Called when make build is run
 .PHONY: gorelease
-gorelease::
-	goreleaser build --rm-dist
+gorelease:: $(GO_RELEASER)
+	goreleaser release --rm-dist
 
 # Called when make release is run
 .PHONY: gobuild
-gobuild::
-	goreleaser release --rm-dist
+gobuild:: $(GO_RELEASER)
+	goreleaser build --rm-dist
