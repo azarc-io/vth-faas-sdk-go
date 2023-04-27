692ef1b402b09cbd30b047e3ecd7321a7c8f6803
Jonathan Langley <jono.langley@azarc.io>
2023-04-27 18:28:24 +0200

    Add ability for codec to marshal raw string and []byte values

--- pkg/codec/codec.go
+++ pkg/codec/codec.go
@@ -66,5 +66,18 @@ func Decode(input any, target any) error {
 		return ErrInvalidOctetStreamType
 	}
 
-	return json.Unmarshal(data, target)
+	if err := json.Unmarshal(data, target); err != nil {
+		// this is fallback for non-json types being set
+		switch target.(type) {
+		case *string:
+			// is a string
+			rv.Elem().Set(reflect.ValueOf(string(data)))
+		case *[]byte:
+			rv.Elem().Set(reflect.ValueOf(data))
+		default:
+			return err
+		}
+	}
+
+	return nil
 }
