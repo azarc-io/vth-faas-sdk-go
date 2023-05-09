8c95ae317a37e08bafa9c2037dd8b97811d6fe74
Jonathan Langley <jono.langley@azarc.io>
2023-05-09 12:42:17 +0200

    Allow Bindable types to return raw bytes

--- pkg/codec/codec.go
+++ pkg/codec/codec.go
@@ -6,6 +6,7 @@ import (
 	"errors"
 	"fmt"
 	"reflect"
+	"regexp"
 	"strings"
 )
 
@@ -24,6 +25,9 @@ const (
 	MimeTypeText        MimeType = "application/text"
 	MimeTypeOctetStream MimeType = "application/octet-stream"
 )
+
+// isBase64Characters checks if a string contains only valid base64 characters
+var validBase64 = regexp.MustCompile(`^[A-Za-z0-9+/]*=?=?$`)
 
 func (mt MimeType) WithType(subType string) MimeType {
 	return MimeType(fmt.Sprintf("%s+%s", mt, strings.ToLower(subType)))
@@ -87,4 +91,49 @@ func Decode(input any, target any) error {
 	}
 
 	return nil
+}
+
+func DecodeToBytes(input any) ([]byte, error) {
+	if input == nil {
+		return nil, nil
+	}
+
+	var err error
+	switch val := input.(type) {
+	case string:
+		input, err = base64.StdEncoding.DecodeString(val)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	data, ok := input.([]byte)
+	if !ok {
+		return nil, ErrInvalidOctetStreamType
+	}
+
+	// check if this is a JSON string instead
+	var nv string
+	if err2 := json.Unmarshal(data, &nv); err2 == nil {
+		// covers test: "json bytes from raw json string"
+		data = []byte(nv)
+	}
+
+	// Check if this is double encoded byte array
+	if IsBase64(string(data)) {
+		if dbl, err := base64.StdEncoding.DecodeString(string(data)); err == nil {
+			data = dbl
+		}
+	}
+
+	return data, nil
+}
+
+// IsBase64 checks if a string is a valid base64 encoded string
+func IsBase64(s string) bool {
+	if len(s)%4 != 0 || !validBase64.MatchString(s) {
+		return false
+	}
+
+	return true
 }
