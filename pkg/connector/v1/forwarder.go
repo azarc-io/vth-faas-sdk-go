87d208f282f23f2da4405550aeca2b16147a20e3
Toka Abramishvili <toka.abramishvili@azarc.io>
2023-06-01 13:24:40 +0400

    SBR-848: Add logs to the forwarder

--- pkg/connector/v1/forwarder.go
+++ pkg/connector/v1/forwarder.go
@@ -19,6 +19,7 @@ type forwarderOption func(forwarder) forwarder
 type forwarder struct {
 	httpClient requestDoer
 	config     *connectorConfig
+	logger     Logger
 }
 
 type forwardData struct {
@@ -48,6 +49,7 @@ func (f *forwarder) Forward(name string, body []byte, headers Headers) (InboundR
 	// TODO: Body must be JSON object for now but we must change to bytes after agent update
 	if len(body) > 0 {
 		if err := json.Unmarshal(body, &map[string]any{}); err != nil {
+			f.logger.Error(err, "request body must be a valid json object")
 			return nil, errors.New("request body must be a valid json object")
 		}
 	}
@@ -64,24 +66,30 @@ func (f *forwarder) Forward(name string, body []byte, headers Headers) (InboundR
 
 	data, err := json.Marshal(req)
 	if err != nil {
+		f.logger.Error(err, "[forwarder] failed to marshal request")
 		return nil, err
 	}
 	request, err := http.NewRequest(http.MethodPost, f.config.Agent.forwarderURL(), bytes.NewReader(data))
 	if err != nil {
+		f.logger.Error(err, "[forwarder] failed to create a new request")
 		return nil, err
 	}
 	request.Header.Set("Content-Type", "application/json")
 	request.Header.Set(agentTokenHeader, f.config.Agent.Token)
+	f.logger.Debug("[forwarder] %s %s: %s", request.Method, request.URL.String(), string(data))
 
 	response, err := f.httpClient.Do(request)
 	if err != nil {
+		f.logger.Error(err, "[forwarder] failed to do the request")
 		return nil, err
 	}
+	body, err = io.ReadAll(response.Body)
+	if err != nil {
+		f.logger.Error(err, "[forwarder] failed to read response body")
+		return nil, err
+	}
+	f.logger.Debug("[forwarder] response body: %s", string(body))
 	if response.StatusCode != http.StatusOK {
-		body, err := io.ReadAll(response.Body)
-		if err != nil {
-			return nil, err
-		}
 		return nil, &HttpError{
 			HttpCode: response.StatusCode,
 			Reason:   response.Status,
@@ -89,8 +97,9 @@ func (f *forwarder) Forward(name string, body []byte, headers Headers) (InboundR
 		}
 	}
 	var resp forwardData
-	err = json.NewDecoder(response.Body).Decode(&resp)
+	err = json.Unmarshal(body, &resp)
 	if err != nil {
+		f.logger.Error(err, "[forwarder] failed to decode response body. body: %s", string(body))
 		return nil, err
 	}
 	return resp, nil
@@ -104,12 +113,22 @@ func newForwarder(config *connectorConfig, opts ...forwarderOption) Forwarder {
 	if fwd.httpClient == nil {
 		fwd.httpClient = http.DefaultClient
 	}
+	if fwd.logger == nil {
+		fwd.logger = noopLogger{}
+	}
 	return &fwd
 }
 
 func withRequestDoer(doer requestDoer) forwarderOption {
 	return func(f forwarder) forwarder {
 		f.httpClient = doer
+		return f
+	}
+}
+
+func withLogger(logger Logger) forwarderOption {
+	return func(f forwarder) forwarder {
+		f.logger = logger
 		return f
 	}
 }
